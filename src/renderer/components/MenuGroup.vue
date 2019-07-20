e2356c62ce395978f043a97364080596f029f414
Nikan Dalvand <nikandalvand@gmail.com>
2019-07-20 14:48:02 +0430

    Created base component for MenuGroup and MenuItem

---
+++ src/renderer/components/MenuGroup.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        MenuGroup
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MenuGroup'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/renderer/components/MenuItem.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Item
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MenuItem'
+}
+</script>
+
+<style>
+
+</style>
