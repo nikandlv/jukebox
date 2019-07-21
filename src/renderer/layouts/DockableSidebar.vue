dd9ee29615f7376b9fa78276664850f98c12c03f
Nikan Dalvand <nikandalvand@gmail.com>
2019-07-21 16:13:57 +0430

    Added handle menu icon and adjusted its logo

--- src/renderer/layouts/DockableSidebar.vue
+++ src/renderer/layouts/DockableSidebar.vue
@@ -1,10 +1,23 @@
 <template>
     <div>
         <section class="handle">
+            <div class="icon">
+                <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 384.97 384.97" style="enable-background:new 0 0 384.97 384.97;" xml:space="preserve">
+                    <g>
+                    <g id="Menu_1_">
+                        <path d="M12.03,120.303h360.909c6.641,0,12.03-5.39,12.03-12.03c0-6.641-5.39-12.03-12.03-12.03H12.03
+                            c-6.641,0-12.03,5.39-12.03,12.03C0,114.913,5.39,120.303,12.03,120.303z"/>
+                        <path d="M372.939,180.455H12.03c-6.641,0-12.03,5.39-12.03,12.03s5.39,12.03,12.03,12.03h360.909c6.641,0,12.03-5.39,12.03-12.03
+                            S379.58,180.455,372.939,180.455z"/>
+                        <path d="M372.939,264.667H132.333c-6.641,0-12.03,5.39-12.03,12.03c0,6.641,5.39,12.03,12.03,12.03h240.606
+                            c6.641,0,12.03-5.39,12.03-12.03C384.97,270.056,379.58,264.667,372.939,264.667z"/>	
+                    </g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g>
+                </svg>
+            </div>
             <Logo :nopadding="true"/>
         </section>
         <section class="sidebar">
-            <Logo />
+            <Logo class="logo" />
             <MenuGroup>
                 <template v-slot:title>Menu</template>
                 <MenuItem to="/artists">
@@ -115,6 +128,19 @@ section.handle
     display: flex
     align-items: center
     padding: 0 1rem
+    .icon
+        margin: 0 10px
+        width: 24px
+        height: 24px
+        padding: 0.8rem
+        cursor: pointer
+        transition: background-color 200ms
+        border-radius: 100%
+        &:hover
+            background-color: rgba(black,0.05)
+        svg
+            width: 24px
+            height: 24px
 section.sidebar
     width: 250px
     max-width: 250px
