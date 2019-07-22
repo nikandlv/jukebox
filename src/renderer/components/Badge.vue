1a644deb73b7c3880bd43982531aa8eb0ca187cb
NoisyBoy-9 <sina.shariati@yahoo.com>
2019-07-23 00:44:12 +0430

    finished the action bar

---
+++ src/renderer/components/Badge.vue
@@ -0,0 +1,37 @@
+<template>
+  <span class="badge__container">
+    <p class="badge__number">
+      <slot></slot>
+    </p>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'Badge'
+}
+</script>
+
+<style lang="sass" scoped>
+.badge
+  &__container
+    // base styles
+    width: 1rem
+    height: 1rem
+    background-color: #336cfb
+    display: flex
+    justify-content: center
+    align-items: center
+    border-radius: 50%
+    border: 2px solid white
+    // positioning according to the the notification icon
+    position: absolute
+    top: -0.2rem
+    right: -.3rem
+  &__number
+    font-size: .8rem
+    color: white
+    font-weight: 600
+    padding: 0 
+    display: inline-block
+</style>
---
+++ src/renderer/components/BaseInput.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="search__container">
+      <input type="search" class="search__input" v-bind="query" :placeholder="placeholder">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseInput',
+  data () {
+    return {
+      query: null
+    }
+  },
+  props: ['placeholder']
+}
+</script>
+
+
+<style lang="sass" scoped>
+.search
+  &__container 
+    padding: 0
+    diplay: flex
+    justify-content: center
+    width: 40%
+
+  &__input
+    width: 90%
+    height: 100%
+    border-radius: 15px
+    padding: .4rem 1.5rem
+    color: lighten(black, 30) 
+    border: 1px solid lighten(grey, 40)
+    margin-right: -2rem
+    background-color: #f9f9f9
+    transition: all .2s ease-in-out
+    
+    &::placeholder
+      color: #a8abb3
+      
+    &:focus
+      outline: none
+      width: 100%
+
+  &__icon
+    width: 1.2rem
+    height: 1.2rem
+    cursor: pointer
+    fill: #3b3c3d
+</style>
+
