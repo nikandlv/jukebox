e374b06c7e73058ac35a730077830673153387a7
Nikan Dalvand <nikandalvand@gmail.com>
2019-07-24 19:36:25 +0430

    Added artwork,title,artist to playerbar

--- src/renderer/layouts/PlayerBar.vue
+++ src/renderer/layouts/PlayerBar.vue
@@ -1,6 +1,10 @@
 <template>
     <section>
-        Player
+        <img class="artwork" src="/static/demo/artwork-4.png" />
+        <div>
+            <p class="title">Perfidia</p>
+            <p class="artist">Nat King Cole</p>
+        </div>
     </section>
 </template>
 
@@ -13,6 +17,16 @@ export default {
 <style lang="sass" scoped>
 section
     display: flex
+    align-items: center
     border-top: 1px solid rgba(0,0,0,0.1)
-    height: 4.5rem
+    height: 84px
+    img.artwork
+        height: 100%
+        width: auto
+        border-radius: 2px
+    p.title,p.artist
+        font-size: .8rem
+        margin: 8px 10px
+    p.artist
+        color: lighten(black,45)
 </style>
