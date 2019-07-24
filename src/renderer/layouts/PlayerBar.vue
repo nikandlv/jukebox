3878619ee442b7568509868480d46abbfe813992
Nikan Dalvand <nikandalvand@gmail.com>
2019-07-24 20:11:17 +0430

    Removed redundent vue-audio-visual setup

--- src/renderer/layouts/PlayerBar.vue
+++ src/renderer/layouts/PlayerBar.vue
@@ -7,10 +7,7 @@
         </div>
         <div class="bars">
             <audio ref="visualizer" autoplay src="/static/demo/music.mp3"></audio>
-                <av-waveform
-      ref-link="visualizer"
-      :playtime-line-width="0" :canv-width="800" :canv-height="64" :playtime="false" :playtime-font-size="0" noplayed-line-color="#336cfb" playtime-line-color="#ABC123" :playtime-slider="true"
-    ></av-waveform>
+                
         </div>
     </section>
 </template>
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,12 +4,10 @@ import axios from 'axios'
 import App from './App'
 import router from './router'
 import store from './store'
-import AudioVisual from 'vue-audio-visual'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
-Vue.use(AudioVisual)
 
 /* eslint-disable no-new */
 new Vue({
