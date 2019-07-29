7d7fbe3fc16fe8a6605886a2fe0350bd5910d006
Nikan Dalvand <nikandalvand@gmail.com>
2019-07-29 22:57:55 +0430

    Added some pages

---
+++ src/renderer/layouts/CollectionPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        CollectionPage
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CollectionPage'
+}
+</script>
+
+<style lang="sass">
+
+</style>
---
+++ src/renderer/layouts/PlaylistPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        PlaylistPage
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PlaylistPage'
+}
+</script>
+
+<style lang="sass">
+
+</style>
