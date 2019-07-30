165913ecf37a9eb9b6707acdff88b96697c67588
Nikan Dalvand <nikandalvand@gmail.com>
2019-07-30 22:53:01 +0430

    Added viewmode button to playerbar

--- src/renderer/layouts/PlayerBar.vue
+++ src/renderer/layouts/PlayerBar.vue
@@ -28,6 +28,9 @@
             <IconButton variant="contained">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M4 18l8.5-6L4 6v12zm9-12v12l8.5-6L13 6z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
             </IconButton>
+            <IconButton variant="contained">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+            </IconButton>
         </div>
     </section>
 </template>
