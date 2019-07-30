8766dc6642153d2fda7fd1c4f4db97f56242c2f9
Nikan Dalvand <nikandalvand@gmail.com>
2019-07-30 22:43:37 +0430

    Created Dialog component

---
+++ src/renderer/components/Dialog.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="dialog" :class="{'open' : open}">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Dialog'
+}
+</script>
+
+<style scoped lang="sass">
+.dialog
+    display: block
+</style>
